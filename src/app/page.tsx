6322b25f109432c00fb26d18b3712498ce95caae
B-Manitas <manitas.bhr@gmail.com>
2025-10-30 11:42:27 +0100

    Add About section to Home component for enhanced information display

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import Hero from 'section/hero/Hero';
 import Background from 'section/background/Background';
 import Projects from 'section/projects/Projects';
 import Footer from 'section/footer/Footer';
+import About from './shared/section/about/About';
 
 export default function Home() {
 	return (
 		<>
 			<Header />
 			<Hero />
+			<About />
 			<Background />
 			<Projects />
 			<Footer />
