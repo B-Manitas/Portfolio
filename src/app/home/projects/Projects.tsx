2f3e2bf427443e147a20a276ff4a626bbf19f76f
B-Manitas <manitas.bhr@gmail.com>
2025-10-30 16:43:26 +0100

    Implement Altair project card with link support and enhance styling

--- src/app/home/projects/Projects.tsx
+++ src/app/home/projects/Projects.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useState, useEffect, useMemo } from 'react';
+import Link from 'next/link';
 import { FiChevronRight } from 'react-icons/fi';
 import ProjectModal from 'component/modal/ProjectModal';
 import projectsJSON from 'data/projects.json';
@@ -53,32 +54,50 @@ export default function Projects() {
 				<div key={group.category} className={styles.categoryBlock}>
 					<h3 className={styles.categoryTitle}>{group.category}</h3>
 					<div className={styles.grid}>
-						{group.projects.map((project) => (
-							<button
-								key={project.id}
-								type="button"
-								className={styles.card}
-								onClick={() => setSelectedProject(project)}
-							>
-								<div className={styles.cardHeader}>
-									<span className={styles.date}>{project.date}</span>
-									<FiChevronRight className={styles.iconLink} />
-								</div>
-
-								<h4 className={styles.projectTitle}>{project.title}</h4>
-								<p className={styles.description}>{project.description}</p>
-
-								{project.technologies?.length && (
-									<div className={styles.techList}>
-										{project.technologies.map((tech) => (
-											<span key={tech} className={styles.techBadge}>
-												{tech}
-											</span>
-										))}
+						{group.projects.map((project) => {
+							return project.page ? (
+								<Link key={project.id} href={project.page} className={styles.card}>
+									<div className={styles.cardHeader}>
+										<span className={styles.date}>{project.date}</span>
+										<FiChevronRight className={styles.iconLink} />
 									</div>
-								)}
-							</button>
-						))}
+									<h4 className={styles.projectTitle}>{project.title}</h4>
+									<p className={styles.description}>{project.description}</p>
+									{project.technologies?.length && (
+										<div className={styles.techList}>
+											{project.technologies.map((tech) => (
+												<span key={tech} className={styles.techBadge}>
+													{tech}
+												</span>
+											))}
+										</div>
+									)}
+								</Link>
+							) : (
+								<button
+									key={project.id}
+									type="button"
+									className={styles.card}
+									onClick={() => setSelectedProject(project)}
+								>
+									<div className={styles.cardHeader}>
+										<span className={styles.date}>{project.date}</span>
+										<FiChevronRight className={styles.iconLink} />
+									</div>
+									<h4 className={styles.projectTitle}>{project.title}</h4>
+									<p className={styles.description}>{project.description}</p>
+									{project.technologies?.length && (
+										<div className={styles.techList}>
+											{project.technologies.map((tech) => (
+												<span key={tech} className={styles.techBadge}>
+													{tech}
+												</span>
+											))}
+										</div>
+									)}
+								</button>
+							);
+						})}
 					</div>
 				</div>
 			))}
